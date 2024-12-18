85ae4fab0f4532c79ac2ec0d7801080ed8bbd037
aulia <rahmanaulia336@gmail.com>
2024-12-18 11:00:39 +0700

    Data Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
 
 function App() {
+  const [search, setsearch] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setsearch(e.target.value);
+      console.log(e);
+  }
+
+  const onClick = (e: SyntheticEvent) => {
+      console.log(e);
+  }
+  
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
       <CardList />
     </div>
   );
