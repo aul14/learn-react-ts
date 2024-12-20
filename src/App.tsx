021b4b59e0fb6459a2c7db2ab7f35f0a09ea923e
aulia <rahmanaulia336@gmail.com>
2024-12-20 09:18:55 +0700

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
       setsearch(e.target.value);
       console.log(e);
   }
 
-  const onClick = async (e: SyntheticEvent) => {
+  const onSearchSubmit = async (e: SyntheticEvent) => {
       const result = await SearchCompanies(search);
 
       if (typeof result == "string") {
@@ -25,11 +25,16 @@ function App() {
       }
       console.log(searchResult);
   }
+
+  const onPortfolioCreate = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(e);
+  }
   
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      <CardList searchResults={searchResult} />
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
       {serverError && <h1>Unable to connect to API</h1>}
     </div>
   );
