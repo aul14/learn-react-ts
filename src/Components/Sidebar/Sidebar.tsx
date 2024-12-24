04a5b8a133ac2739ca42bd4688812ff1a105fa2a
aulia <rahmanaulia336@gmail.com>
2024-12-24 16:27:54 +0700

    historical dividend

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { SlGraph } from "react-icons/sl";
 import {
   FaHome,
   FaMoneyBill,
@@ -47,6 +48,13 @@ const Sidebar = (props: Props) => {
             >
               <FaMoneyBill />
               <h6 className="ml-3">Cashflow Statement</h6>
+            </Link>
+            <Link
+              to="historical-dividend"
+              className="flex md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+            >
+              <SlGraph />
+              <h6 className="ml-3">Historical Dividend</h6>
             </Link>
           </div>
         </div>
