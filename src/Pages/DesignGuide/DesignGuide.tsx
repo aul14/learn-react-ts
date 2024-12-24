6f61357e9986166ee3d31b7e8663d42c02d79021
aulia <rahmanaulia336@gmail.com>
2024-12-24 11:59:59 +0700

    Company Profile/TTM

--- src/Pages/DesignGuide/DesignGuide.tsx
+++ src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Table from "../../Components/Table/Table";
 import RatioList from "../../Components/RatioList/RatioList";
+import { CompanyKeyMetrics } from "../../company";
+import { testIncomeStatementData } from "../../Components/Table/testData";
 
 type Props = {};
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
 const DesignGuide = (props: Props) => {
   return (
     <>
       <h1>Finshark Design Guide</h1>
-      <RatioList />
+      <RatioList data={testIncomeStatementData} config={tableConfig} />
       <Table />
       <h3>
         This is Finshark's design guide. This is where we well house various
--- src/api.tsx
+++ src/api.tsx
@@ -1,33 +1,46 @@
-import axios from 'axios'
-import { CompanyProfile, CompanySearch } from './company'
+import axios from "axios";
+import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
-export const searchCompanies = async (query:string) => {
-    try {
-        const data = await axios.get<SearchResponse>(`https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`);
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
 
-        return data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.log("Error message: ", error.message);
-            return error.message;
-        } else {
-            console.log("Unexpected error: ", error);
-            return "An expected error has occured";
-        }
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("Error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("Unexpected error: ", error);
+      return "An expected error has occured";
     }
-}
+  }
+};
 
 export const getCompanyProfile = async (query: string) => {
-    try {
-        const data = await axios.get<CompanyProfile[]>(
-            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
-        );
-        return data;
-    } catch (error: any) {
-        console.log("error message: ", error.message);
-    }
-}
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getKeyMetrics = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyKeyMetrics[]>(
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
