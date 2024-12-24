5d117c2cc6540692b2485d15e9de86867f6bdd17
aulia <rahmanaulia336@gmail.com>
2024-12-24 11:13:27 +0700

    Table

---
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>Finshark Design Page</h1>
+      <h2>
+        This is Finshark's design page. This is where we well house various
+        design aspects of the app
+      </h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
